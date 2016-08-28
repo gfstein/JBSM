95c4eeba79bece25da2350c48b642cc2a674948b
Willian <willianw94@hotmail.com>
2016-08-28 01:37:38 -0300

    3

--- src/br/com/jbsm/model/Person.java
+++ src/br/com/jbsm/model/Person.java
@@ -1,8 +1,44 @@
 package br.com.jbsm.model;
 
-public class Person {
+import com.sun.istack.internal.NotNull;
 
+import javax.persistence.Id;
+
+public class Person {
+    @Id
+    @NotNull
+    private Integer id;
+
+    @NotNull
     private String nome;
+
+    @NotNull
     private String cpf;
 
+    public Person() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
